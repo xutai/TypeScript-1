tbvjaos510/TypeScript

Clean resolutions from tsbuild info

src/compiler/tsbuildPublic.ts
@@ -133,8 +133,10 @@ namespace ts {
     export interface SolutionBuilder<T extends BuilderProgram> {
         build(project?: string, cancellationToken?: CancellationToken): ExitStatus;
         clean(project?: string): ExitStatus;
+        cleanResolutions(project?: string): ExitStatus;
         buildReferences(project: string, cancellationToken?: CancellationToken): ExitStatus;
         cleanReferences(project?: string): ExitStatus;
+        cleanResolutionsOfReferences(project?: string): ExitStatus;
         getNextInvalidatedProject(cancellationToken?: CancellationToken): InvalidatedProject<T> | undefined;
 
         // Currently used for testing but can be made public if needed:
@@ -1726,6 +1728,38 @@ namespace ts {
         return ExitStatus.Success;
     }
 
+    function cleanResolutions(state: SolutionBuilderState, project?: string, onlyReferences?: boolean) {
+        const buildOrder = getBuildOrderFor(state, project, onlyReferences);
+        if (!buildOrder) return ExitStatus.InvalidProject_OutputsSkipped;
+
+        if (isCircularBuildOrder(buildOrder)) {
+            reportErrors(state, buildOrder.circularDiagnostics);
+            state.host.reportErrorSummary?.(getErrorCountForSummary(buildOrder.circularDiagnostics));
+            return ExitStatus.ProjectReferenceCycle_OutputsSkipped;
+        }
+
+        let diagnostics = 0;
+        for (const proj of buildOrder) {
+            const resolvedPath = toResolvedConfigFilePath(state, proj);
+            const parsed = parseConfigFile(state, proj, resolvedPath);
+            if (parsed) {
+                diagnostics += cleanResolutionsOfTsBuildInfoAndReportError(
+                    parsed.options,
+                    state.compilerHost,
+                    err => state.host.reportDiagnostic(err),
+                    state.writeFileName,
+                );
+            }
+            else {
+                // File has gone missing; fine to ignore here
+                reportParseConfigFileDiagnostic(state, resolvedPath);
+            }
+        }
+
+        state.host.reportErrorSummary?.(diagnostics);
+        return diagnostics ? ExitStatus.DiagnosticsPresent_OutputsGenerated : ExitStatus.Success;
+    }
+
     function invalidateProject(state: SolutionBuilderState, resolved: ResolvedConfigFilePath, reloadLevel: ConfigFileProgramReloadLevel) {
         // If host implements getParsedCommandLine, we cant get list of files from parseConfigFileHost
         if (state.host.getParsedCommandLine && reloadLevel === ConfigFileProgramReloadLevel.Partial) {
@@ -1884,8 +1918,10 @@ namespace ts {
         return {
             build: (project, cancellationToken) => build(state, project, cancellationToken),
             clean: project => clean(state, project),
+            cleanResolutions: project => cleanResolutions(state, project),
             buildReferences: (project, cancellationToken) => build(state, project, cancellationToken, /*onlyReferences*/ true),
             cleanReferences: project => clean(state, project, /*onlyReferences*/ true),
+            cleanResolutionsOfReferences: project => cleanResolutions(state, project, /*onlyReferences*/ true),
             getNextInvalidatedProject: cancellationToken => {
                 setupInitialBuild(state, cancellationToken);
                 return getNextInvalidatedProject(state, getBuildOrder(state), /*reportQueue*/ false);

src/compiler/watch.ts
@@ -233,6 +233,29 @@ namespace ts {
         return ExitStatus.Success;
     }
 
+    export interface CleanResolutionsOfTsBuildInfoAndReportErrorHost extends CleanResolutionsOfTsBuildInfoHost {
+        getCurrentDirectory(): string;
+    }
+    export function cleanResolutionsOfTsBuildInfoAndReportError(
+        options: CompilerOptions,
+        host: CleanResolutionsOfTsBuildInfoAndReportErrorHost,
+        reportDiagnostic: DiagnosticReporter,
+        writeFileName?: (s: string) => void,
+        reportSummary?: ReportEmitErrorSummary,
+    ): number {
+        const { emittedFiles, diagnostics } = cleanResolutionsOfTsBuildInfo(options, host);
+        diagnostics.forEach(reportDiagnostic);
+        if (writeFileName) {
+            const currentDir = host.getCurrentDirectory();
+            forEach(emittedFiles, file => {
+                const filepath = getNormalizedAbsolutePath(file, currentDir);
+                writeFileName(`TSFILE: ${filepath}`);
+            });
+        }
+        reportSummary?.(diagnostics.length);
+        return diagnostics.length;
+    }
+
     export const noopFileWatcher: FileWatcher = { close: noop };
     export const returnNoopFileWatcher = () => noopFileWatcher;
 

xutai/TypeScript-1

src/compiler/tsbuildPublic.ts
@@ -134,8 +134,10 @@ namespace ts {
     export interface SolutionBuilder<T extends BuilderProgram> {
         build(project?: string, cancellationToken?: CancellationToken, writeFile?: WriteFileCallback, getCustomTransformers?: (project: string) => CustomTransformers): ExitStatus;
         clean(project?: string): ExitStatus;
+        cleanResolutions(project?: string): ExitStatus;
         buildReferences(project: string, cancellationToken?: CancellationToken, writeFile?: WriteFileCallback, getCustomTransformers?: (project: string) => CustomTransformers): ExitStatus;
         cleanReferences(project?: string): ExitStatus;
+        cleanResolutionsOfReferences(project?: string): ExitStatus;
         getNextInvalidatedProject(cancellationToken?: CancellationToken): InvalidatedProject<T> | undefined;
 
         // Currently used for testing but can be made public if needed:
@@ -1724,6 +1726,38 @@ namespace ts {
         return ExitStatus.Success;
     }
 
+    function cleanResolutions(state: SolutionBuilderState, project?: string, onlyReferences?: boolean) {
+        const buildOrder = getBuildOrderFor(state, project, onlyReferences);
+        if (!buildOrder) return ExitStatus.InvalidProject_OutputsSkipped;
+
+        if (isCircularBuildOrder(buildOrder)) {
+            reportErrors(state, buildOrder.circularDiagnostics);
+            state.host.reportErrorSummary?.(getErrorCountForSummary(buildOrder.circularDiagnostics));
+            return ExitStatus.ProjectReferenceCycle_OutputsSkipped;
+        }
+
+        let diagnostics = 0;
+        for (const proj of buildOrder) {
+            const resolvedPath = toResolvedConfigFilePath(state, proj);
+            const parsed = parseConfigFile(state, proj, resolvedPath);
+            if (parsed) {
+                diagnostics += cleanResolutionsOfTsBuildInfoAndReportError(
+                    parsed.options,
+                    state.compilerHost,
+                    err => state.host.reportDiagnostic(err),
+                    state.write,
+                );
+            }
+            else {
+                // File has gone missing; fine to ignore here
+                reportParseConfigFileDiagnostic(state, resolvedPath);
+            }
+        }
+
+        state.host.reportErrorSummary?.(diagnostics);
+        return diagnostics ? ExitStatus.DiagnosticsPresent_OutputsGenerated : ExitStatus.Success;
+    }
+
     function invalidateProject(state: SolutionBuilderState, resolved: ResolvedConfigFilePath, reloadLevel: ConfigFileProgramReloadLevel) {
         // If host implements getParsedCommandLine, we cant get list of files from parseConfigFileHost
         if (state.host.getParsedCommandLine && reloadLevel === ConfigFileProgramReloadLevel.Partial) {
@@ -1898,8 +1932,10 @@ namespace ts {
         return {
             build: (project, cancellationToken, writeFile, getCustomTransformers) => build(state, project, cancellationToken, writeFile, getCustomTransformers),
             clean: project => clean(state, project),
+            cleanResolutions: project => cleanResolutions(state, project),
             buildReferences: (project, cancellationToken, writeFile, getCustomTransformers) => build(state, project, cancellationToken, writeFile, getCustomTransformers, /*onlyReferences*/ true),
             cleanReferences: project => clean(state, project, /*onlyReferences*/ true),
+            cleanResolutionsOfReferences: project => cleanResolutions(state, project, /*onlyReferences*/ true),
             getNextInvalidatedProject: cancellationToken => {
                 setupInitialBuild(state, cancellationToken);
                 return getNextInvalidatedProject(state, getBuildOrder(state), /*reportQueue*/ false);

src/compiler/watch.ts
@@ -392,6 +392,29 @@ namespace ts {
         return ExitStatus.Success;
     }
 
+    export interface CleanResolutionsOfTsBuildInfoAndReportErrorHost extends CleanResolutionsOfTsBuildInfoHost {
+        getCurrentDirectory(): string;
+    }
+    export function cleanResolutionsOfTsBuildInfoAndReportError(
+        options: CompilerOptions,
+        host: CleanResolutionsOfTsBuildInfoAndReportErrorHost,
+        reportDiagnostic: DiagnosticReporter,
+        write?: (s: string) => void,
+        reportSummary?: ReportEmitErrorSummary,
+    ): number {
+        const { emittedFiles, diagnostics } = cleanResolutionsOfTsBuildInfo(options, host);
+        diagnostics.forEach(reportDiagnostic);
+        if (write) {
+            const currentDir = host.getCurrentDirectory();
+            forEach(emittedFiles, file => {
+                const filepath = getNormalizedAbsolutePath(file, currentDir);
+                write(`TSFILE: ${filepath}`);
+            });
+        }
+        reportSummary?.(diagnostics.length);
+        return diagnostics.length;
+    }
+
     export const noopFileWatcher: FileWatcher = { close: noop };
     export const returnNoopFileWatcher = () => noopFileWatcher;
 